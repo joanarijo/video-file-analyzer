44959ccaa6db5d84943b55c6b135a03068a2fdb6
joanarijo <joana.r@tux-gill.pt>
2021-11-09 12:16:53 +0000

    ipc events

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
     console.log('App is ready!');
-    const mainWindow= new BrowserWindow({});
+    mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true , contextIsolation: false } });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('videosubmit', () => {
-    
+ipcMain.on('videosubmit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+       mainWindow.webContents.send('videometadata', metadata.format.duration);
+    });
 });
